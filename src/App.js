49a46c6b9fabdb9bf57376f1a22dd927a5d258a9
Ali Zain <vin.alizain@gmail.com>
2021-04-03 00:37:15 +0500

    Crud App
    React Redux Firebase Crud App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import { useSelector } from "react-redux";
 
 function App() {
+  const userData = useSelector((state) => state.auth);
+  const { user } = userData;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {/* <>{!user ? <Login /> : <Home />}</> */}
+        {!user ? (
+          <>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/home">
+              <Login />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/">
+              <Login />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
   );
 }
 
